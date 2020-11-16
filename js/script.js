cbc80bb5374f88f1a8b16c1cbfab3bf7f2eaf18a
Alessandro <aleneri242@gmail.com>
2020-11-16 15:44:44 +0100

    primo commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Lista Cognomi
+// Chiedere all’utente il cognome,
+// inserirlo in un array con altri cognomi (Es: 'Pesce', ‘Castenetto’, 'Passaro', 'Grimaldi', ‘Rossi’, ‘Verdi’, ‘Bianchi’).
+// Stampa la lista ordinata alfabeticamente.
+// Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova.
+// Consigli del giorno:
+// Consultiamo la documentazione W3Schools o MDN per trovare i metodi javascript che possono esserci utili.
+
+var listaCognomi = ["Neri", "Calvieri", "Romiti", "Bervicato", "Busellato", "Ghilardi"];
+console.log(listaCognomi);
+
+var nuovoCognome = prompt('Inserisci il tuo cognome');
+listaCognomi.push(nuovoCognome);
